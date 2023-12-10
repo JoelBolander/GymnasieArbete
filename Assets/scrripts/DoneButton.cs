21bfdd6ea2cb1f8a105275e9a2bcafd156426203
JoelBolander <joel.bolander@skola.taby.se>
2023-12-10 21:01:56 +0100

    start menu

---
+++ Assets/scrripts/DoneButton.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DoneButton : MonoBehaviour
+{
+    [SerializeField] private GameObject selected;
+
+    public void ClickEvent()
+    {
+        selected.GetComponent<TextMeshProUGUI>().text = this.gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text;
+    }
+}
